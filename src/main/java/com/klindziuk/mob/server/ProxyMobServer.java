c414a686665809fadd1540ee21746ea8e1243960
Pavel Klindziuk <pavel_klindziuk@epam.com>
2017-07-23 19:55:37 +0300

    ca root sertificates

--- src/main/java/com/klindziuk/mob/server/ProxyMobServer.java
+++ src/main/java/com/klindziuk/mob/server/ProxyMobServer.java
@@ -1,14 +1,17 @@
 package com.klindziuk.mob.server;
 
+import java.io.File;
+
 import net.lightbody.bmp.BrowserMobProxy;
 import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.mitm.RootCertificateGenerator;
+import net.lightbody.bmp.mitm.keys.ECKeyGenerator;
+import net.lightbody.bmp.mitm.manager.ImpersonatingMitmManager;
 import net.lightbody.bmp.proxy.CaptureType;
 
 public final class ProxyMobServer {
-	@SuppressWarnings("unused")
 	private static final String ANY_URL_PATTERN = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-	@SuppressWarnings("unused")
-	private static final String GOOGLE_URL  = "http://google.com";
+	private static final String GOOGLE_URL  = "https://google.com:443";
 	private static ProxyMobServer instance;
 	private BrowserMobProxy proxy;
 	
@@ -21,9 +24,11 @@ public final class ProxyMobServer {
 
 	private ProxyMobServer() {
 		proxy = new BrowserMobProxyServer();
+		proxy.setTrustAllServers(true);
+		proxy.setMitmManager(getMitmManager());
 		proxy.start(8787);
-	// for HTTPS requests BMP cannot modify the host and port, since it is always reusing a persistent connection.
-	//	proxy.rewriteUrl(ANY_URL_PATTERN, GOOGLE_URL);
+	 // for HTTPS requests BMP cannot modify the host and port, since it is always reusing a persistent connection.
+	 //	proxy.rewriteUrl(ANY_URL_PATTERN, GOOGLE_URL);
 		proxy.enableHarCaptureTypes(CaptureType.REQUEST_CONTENT, CaptureType.RESPONSE_CONTENT);
 		proxy.addResponseFilter(new ProxyResponseFilter());
 	}
@@ -31,4 +36,19 @@ public final class ProxyMobServer {
 	public BrowserMobProxy getBrowserMobProxy() {
 		return proxy;
 	}
+	
+	private ImpersonatingMitmManager getMitmManager() {
+		RootCertificateGenerator ecRootCertificateGenerator = RootCertificateGenerator.builder()
+                .keyGenerator(new ECKeyGenerator())    
+                .build();
+
+        ecRootCertificateGenerator.saveRootCertificateAsPemFile(new File("my-dynamic-ca.cer"));
+        ecRootCertificateGenerator.savePrivateKeyAsPemFile(new File("my-ec-private-key.pem"), "secretPassword");
+        ImpersonatingMitmManager mitmManager = ImpersonatingMitmManager.builder()
+                .rootCertificateSource(ecRootCertificateGenerator)
+                .serverKeyGenerator(new ECKeyGenerator())
+                .build();
+	           
+	    return mitmManager;
+ 	}
 }
